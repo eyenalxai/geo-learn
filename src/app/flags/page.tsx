c8d4f40a7ea073dff88cfc78dfb1a25c14eccbc4
Sax Dhiut <mail@iyxeyl.me>
2025-03-30 14:38:12 +0400

    feat: ensure loading state is set when clearing selected country code

--- src/app/flags/page.tsx
+++ src/app/flags/page.tsx
@@ -23,10 +23,9 @@ export default function Page() {
 	})
 
 	useEffect(() => {
-		setLoadingImage(true)
-
 		if (selectedCode) {
 			const timer = setTimeout(() => {
+				setLoadingImage(true)
 				setSelectedCode(null)
 				queryClient.invalidateQueries({ queryKey: ["countries"] })
 			}, 750)
