505b516351f11634938c34a88d42c3fab5f1a8e6
Sax Dhiut <mail@iyxeyl.me>
2025-03-30 12:57:25 +0400

    feat: refactor country selection to use @tanstack/react-query for data fetching

--- src/app/flags/page.tsx
+++ src/app/flags/page.tsx
@@ -2,25 +2,47 @@
 
 import { Button } from "@/components/ui/button"
 import { getRandomCountries } from "@/lib/random-country"
-import { useEffect, useState } from "react"
+import { cn } from "@/lib/utils"
+import { useQuery, useQueryClient } from "@tanstack/react-query"
+import Image from "next/image"
 
 export default function Page() {
-	const [countries, setCountries] =
-		useState<ReturnType<typeof getRandomCountries>>()
+	const queryClient = useQueryClient()
 
-	const loadNewCountries = () => {
-		setCountries(getRandomCountries(/* pass your countries object here */))
-	}
+	const {
+		data: countries,
+		isPending,
+		isError
+	} = useQuery({
+		queryKey: ["countries"],
+		queryFn: getRandomCountries
+	})
 
-	useEffect(() => {
-		setCountries(getRandomCountries())
-	}, [])
+	if (isPending || isError) return null
+
+	const correctCountry = countries.find(
+		(country) => country.isCorrect
+	) as ReturnType<typeof getRandomCountries>[0]
 
 	return (
 		<div>
-			{JSON.stringify(countries)}
-			<div>Hellos</div>
-			<Button onClick={loadNewCountries}>New</Button>
+			<div className={cn("relative", "h-24", "w-12")}>
+				<Image
+					className={cn("object-contain")}
+					src={`/flags/${correctCountry.code.toLowerCase()}.webp`}
+					alt={"flag"}
+					fill
+				/>
+			</div>
+			<Button
+				onClick={() =>
+					queryClient.invalidateQueries({
+						queryKey: ["countries"]
+					})
+				}
+			>
+				New
+			</Button>
 		</div>
 	)
 }
